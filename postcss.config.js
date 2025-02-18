eea42fe2808388d3e8a6cfc9c9dc0a0380b0e48f
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2025-02-18 23:45:48 +0200

    Deployment commit

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import autoprefixer from "autoprefixer";
-import cssnanoPlugin from "cssnano";
-import pxtorem from "postcss-pxtorem";
-
-export const plugins = [
-  autoprefixer,
-  cssnanoPlugin,
-  pxtorem({ propList: ["*"] }),
-];
